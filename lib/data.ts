d9b3de90ffaab0815c5e74cf94ad753c370ae4a1
doanquangdungwork-rgb <doanquangdung.work@gmail.com>
2026-09-16 17:13:57 +0700

    Wire DeductiveTest2-4 into shared data

--- lib/data.ts
+++ lib/data.ts
@@ -1,13 +1,19 @@
 import questions from "../data/questions.json";
 import deductiveTest1 from "../data/deductive-test1.json";
+import { questions as deductiveTest2 } from "../data/deductive-test2";
+import { questions as deductiveTest3 } from "../data/deductive-test3";
+import { questions as deductiveTest4 } from "../data/deductive-test4";
 import catalog from "../data/catalog.json";
 
-export type Question = (typeof questions)[number] | (typeof deductiveTest1)[number];
+export type Question = (typeof questions)[number] | (typeof deductiveTest1)[number] | (typeof deductiveTest2)[number] | (typeof deductiveTest3)[number] | (typeof deductiveTest4)[number];
 export type Catalog = typeof catalog;
 
 const baseQuestions = questions as Question[];
 const test030Questions = deductiveTest1 as Question[];
-export const allQuestions = [...baseQuestions.filter((q: any) => q.testId !== "TEST_030"), ...test030Questions];
+const test031Questions = deductiveTest2.map(([number, t, o, a]) => ({ id: `TEST_031_Q${number}`, testId: "TEST_031", number, p: "deductive", s: "deductive_reasoning", t, o, a } as Question));
+const test032Questions = deductiveTest3.map(([number, t, o, a]) => ({ id: `TEST_032_Q${number}`, testId: "TEST_032", number, p: "deductive", s: "deductive_reasoning", t, o, a } as Question));
+const test033Questions = deductiveTest4.map(([number, t, o, a]) => ({ id: `TEST_033_Q${number}`, testId: "TEST_033", number, p: "deductive", s: "deductive_reasoning", t, o, a } as Question));
+export const allQuestions = [...baseQuestions.filter((q: any) => !["TEST_030", "TEST_031", "TEST_032", "TEST_033"].includes(q.testId)), ...test030Questions, ...test031Questions, ...test032Questions, ...test033Questions];
 export const appCatalog = catalog as Catalog;
 
 export const pillarMap: Record<string, string> = {
